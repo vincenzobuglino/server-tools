7b24e72d162a2c08163d5dd2674b611ac75d0bd4
Rocío Vega <rov@adhoc.com.ar>
2024-01-31 08:55:49 -0300

    [MIG] base_name_search_improved: Migration to 17.0

--- base_name_search_improved/tests/test_name_search.py
+++ base_name_search_improved/tests/test_name_search.py
@@ -7,7 +7,7 @@ from odoo.tests.common import TransactionCase, tagged
 @tagged("post_install", "-at_install")
 class NameSearchCase(TransactionCase):
     def setUp(self):
-        super(NameSearchCase, self).setUp()
+        super().setUp()
         phone_field = self.env.ref("base.field_res_partner__phone")
         model_partner = self.env.ref("base.model_res_partner")
         model_partner.name_search_ids = phone_field
@@ -35,37 +35,35 @@ class NameSearchCase(TransactionCase):
 
     def test_RelevanceOrderedResults(self):
         """Return results ordered by relevance"""
-        res = self.Partner.name_search("555 777")
+        res = self.Partner._name_search("555 777")
+        self.assertEqual(res[0], self.partner1.id, "Match full string honoring spaces")
         self.assertEqual(
-            res[0][0], self.partner1.id, "Match full string honoring spaces"
+            res[1], self.partner2.id, "Match words honoring order of appearance"
         )
         self.assertEqual(
-            res[1][0], self.partner2.id, "Match words honoring order of appearance"
-        )
-        self.assertEqual(
-            res[2][0],
+            res[2],
             self.partner3.id,
             "Match all words, regardless of order of appearance",
         )
 
     def test_NameSearchMustMatchAllWords(self):
         """Must Match All Words"""
-        res = self.Partner.name_search("ulm aaa 555 777")
+        res = self.Partner._name_search("ulm aaa 555 777")
         self.assertFalse(res)
 
     def test_NameSearchDifferentFields(self):
         """Must Match All Words"""
-        res = self.Partner.name_search("ulm 555 777")
+        res = self.Partner._name_search("ulm 555 777")
         self.assertEqual(len(res), 1)
 
     def test_NameSearchDomain(self):
         """Must not return a partner with parent"""
-        res = self.Partner.name_search("Edward Foster")
+        res = self.Partner._name_search("Edward Foster")
         self.assertFalse(res)
 
     def test_MustHonorDomain(self):
         """Must also honor a provided Domain"""
-        res = self.Partner.name_search("+351", args=[("vat", "=", "3333")])
+        res = self.Partner._name_search("+351", domain=[("vat", "=", "3333")])
         gambulputty = self.partner3.id
         self.assertEqual(len(res), 1)
-        self.assertEqual(res[0][0], gambulputty)
+        self.assertEqual(res[0], gambulputty)
